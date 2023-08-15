53452cb7bfd810599d2c57718cca4afa54e16604
myeanji <hyean03@naver.com>
2023-08-16 05:15:22 +0900

    현지 : 라우팅 추가

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import {
+  BrowserRouter as Router,
+  Routes, //
+  Route,  //path의 경로에 렌더링 해줌
+  Link //라우터에서 다른 라우터로 새로고침 필요 없이 갈 수 있게 해줌
+} from "react-router-dom";
+
 import './App.css';
 // 이런식으로 자신의 js파일을 import해서 아래처럼 추가시키면 됩니다!
+// 저 같은 경우는 function StudyMain에 전부 모아둬서 해당 함수를 추가시켰습니다.
 import { StudyMain } from './StudyList/StudyMain';
 
 function App() {
   return (
-    <StudyMain></StudyMain>
+    <Router>
+    <Routes>
+      <Route path="/studymain" element={<StudyMain />} /> {/*스터디 목록*/}
+      {/* path는 http://localhost:3000/studymain <- 이런식으로 :3000 뒤에 붙일 경로를 마음대로 지으면 됩니다. */}
+      {/* 하다가 잘 모르겠으면 카톡 보내주셔도 됩니다 -현지- */}
+    </Routes>
+    </Router>
   );
 }
 
