78d9f0cd3168ce5be1b66c24d1580d7146f0d4fa
ozvshhh <dlgkdms4660@gmail.com>
2023-08-15 21:27:32 +0900

    하은: 브런치 커밋 테스트

---
+++ src/study_detail/study_detail.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import 'src/study_detail/study_detail.css';
+
+function Study_detail(){
+    return(<>
+        
+    </>)
+
+}
+
+function Title(){
+
+}
+
+function Contents(){
+
+}
+
+function Sidebar(){
+    
+}
+
+function View() {
+
+}
